9ee315300b5208b5fa218ab63135993cfc729dd9
Jamie Atyeo <jamie.atyeo@hootsuite.com>
2025-02-25 22:15:30 -0800

    create board and add mouseover effect

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const setupBoard = () => {
     for (let i = 0; i < 16; i++) {
         const row = document.createElement("div");
         row.className = `row${i}`;
+        row.style.display = `flex`;
+        row.style.flexDirection = `row`;
+        row.style.justifyContent = `center`;
+        row.style.alignItems = `center`;
+        row.style.flex = `1 1 auto`;
         rows.push(row);
     }
 
@@ -12,8 +17,21 @@ const setupBoard = () => {
         for (let i = 0; i < 16; i++) {
             const column = document.createElement("div");
             column.className = `column${i}`;
+            column.style.border = `2px black`;
+            column.style.justifyContent = `center`;
+            column.style.alignItems = `center`;
+            column.style.maxHeight = `30px`;
+            column.style.maxWidth = `30px`;
+            column.style.minHeight = `30px`;
+            column.style.minWidth = `30px`;
+            column.style.flex = `1 1 auto`;
+            column.addEventListener("mouseover", () => {
+                column.style.backgroundColor = `black`;
+            });
             row.appendChild(column);
         }
         container.appendChild(row);
     });
-}
+};
+
+setupBoard();
